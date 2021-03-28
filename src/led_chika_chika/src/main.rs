1ab70fde3da716db6222f62f0f3af455d1a9dd78
S-tatsuya_raspberry <s.tatsuya0123@gmail.com>
2021-03-28 17:08:29 +0900

    LEDチカチカ(0.5sec)

--- src/led_chika_chika/src/main.rs
+++ src/led_chika_chika/src/main.rs
@@ -5,16 +5,30 @@ use std::time::Duration;
 use rppal::gpio::Gpio;
 use rppal::system::DeviceInfo;
 
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 const GPIO_LED: u8 = 25;
 
 fn main() -> Result<(), Box<dyn Error>> {
-	println!("Blinking an LED on a {}." , DeviceInfo::new()?.model());
+	let running = Arc::new(AtomicBool::new(true));
+	let r = running.clone();
+
+	ctrlc::set_handler(move || {
+		r.store(false, Ordering::SeqCst);
+	}).expect("Error setting Ctrl-C handler");
+
+	println!("Blinking an LED Chika Chika a {}." , DeviceInfo::new()?.model());
 
 	let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
 
-	pin.set_high();
-	thread::sleep(Duration::from_millis(500));
-	pin.set_low();
+	while running.load(Ordering::SeqCst) {
+		pin.set_high();
+		thread::sleep(Duration::from_millis(500));
+		pin.set_low();
+		thread::sleep(Duration::from_millis(500));
+	}
 
+	pin.set_low();
 	Ok(())
 }
